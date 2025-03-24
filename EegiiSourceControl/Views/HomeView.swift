ee64cc06e6ee96de3d3609b6cc6933a2efcb6df8
Tsetseg219 <enkhtsetseg.unurbayar@techconsulting.tech>
2025-03-24 14:43:04 -0400

    Added Home view modified the Text "Home View!"

---
+++ EegiiSourceControl/Views/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  EegiiSourceControl
+//
+//  Created by Enkhtsetseg Unurbayar on 3/24/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View!")
+        
+    }
+}
+
+#Preview {
+    HomeView()
+}
