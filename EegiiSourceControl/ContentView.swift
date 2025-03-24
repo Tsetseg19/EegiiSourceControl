76b5d168560c573d6d807cc6c6ae5e35928d45bb
Tsetseg219 <enkhtsetseg.unurbayar@techconsulting.tech>
2025-03-24 15:14:03 -0400

    [Feature] Discription: Added Click me! button

--- EegiiSourceControl/ContentView.swift
+++ EegiiSourceControl/ContentView.swift
@@ -15,6 +15,11 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
             Text("Egii's source control")
+            Button("Click me!"){
+                
+            }
+            .background(Color.red)
+
            
         }
         .padding()
