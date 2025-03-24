b264fd87e662d224d9aa3ebd2c27e3556b71faad
Tsetseg219 <enkhtsetseg.unurbayar@techconsulting.tech>
2025-03-24 15:08:12 -0400

    [Clean] Add commit message documentation

--- EegiiSourceControl/Views/HomeView.swift
+++ EegiiSourceControl/Views/HomeView.swift
@@ -5,6 +5,24 @@
 //  Created by Enkhtsetseg Unurbayar on 3/24/25.
 //
 
+/*
+ COMMIT MESSAGES
+ NEW FEATURE:
+ [FEATURE] Description of the freature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ BUT NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ MONDANE TASKS:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ 
+ */
 import SwiftUI
 
 struct HomeView: View {
