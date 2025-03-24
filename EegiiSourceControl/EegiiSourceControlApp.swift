7cf40b7bd74fda34b91c81a3fcad6d3d0e0e60c1
Tsetseg219 <enkhtsetseg.unurbayar@techconsulting.tech>
2025-03-24 11:06:53 -0400

    Initial Commit

---
+++ EegiiSourceControl/EegiiSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  EegiiSourceControlApp.swift
+//  EegiiSourceControl
+//
+//  Created by Enkhtsetseg Unurbayar on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct EegiiSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
