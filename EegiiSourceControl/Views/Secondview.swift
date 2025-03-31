9370a783661f9815f085cce42c84c9ea683948db
Tsetseg219 <enkhtsetseg.unurbayar@techconsulting.tech>
2025-03-31 11:56:12 -0400

    [Feature] Scondview added Text and Click me Button features added.

---
+++ EegiiSourceControl/Views/Secondview.swift
@@ -0,0 +1,18 @@
+
+
+
+import SwiftUI
+
+struct SecondView {
+    
+    var body: some View {
+        
+        HStack{
+            
+            Text("Second View")
+            Button("Click me"){
+                
+            }
+        }
+    }
+}
